8092a7e23e95e814e87c12a2df63650cccb22442
ElodieFerreira <elodief17@hotmail.fr>
2019-03-08 21:56:20 +0100

    mise à jour

--- src/jeu/Joueur.java
+++ src/jeu/Joueur.java
@@ -5,14 +5,25 @@ import java.util.HashMap;
 
 public class Joueur {
 	private String nom;
-	private HashMap<String,Objets> inventaires;
-	private Integer nombreDeTeleportation;
-	private Integer nombreDeTeleportationMax;
+	private ArrayList<Objets> inventaire;
 	private Integer pointdAttaque;
 	private Integer pointDeVie;
 	
 	public Joueur(String nom) {
 		this.nom = nom;
+		inventaire = new ArrayList<Objets>();
+		
+	}
+	public void prendreObjet(Objets A)
+	{
+		inventaire.add(A);
+		System.out.println();
+	}
+	public Objets donnerObjet(int index)
+	{	
+		Objets obj =inventaire.get(index);
+		inventaire.remove(index);
+		return obj;	
 	}
 	
 	
