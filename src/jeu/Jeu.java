fc2706cfb3f25dc9f4857ca2b59c4327b5d09bb1
PommeRouge <elodief17@hotmail.fr>
2019-03-24 16:10:12 +0100

    Doc jeu + commentaire

--- src/jeu/Jeu.java
+++ src/jeu/Jeu.java
@@ -17,45 +17,60 @@ public class Jeu implements Serializable {
     public synchronized Partie getPartie() {
     	return partie;
     }
-    public void setPartie(Partie lastPartie) {
-    	this.partie = lastPartie;
+    /** Restaure l'ancienne partie
+     * @param Partie récupérer par la sauvegarde
+     */
+    public void restaurePartie(Partie partiePrécédente) {
+    	this.partie = partiePrécédente;
     }
     public void setGUI( GUI g) { gui = g; }
+    /**
+     * Lance le début du jeu en vérifiant si il existe une sauvegarde
+     */
     public void lancerDebutJeu() {
+    	// Affecte le jeu de ThreadLauncher avec l'objet courant.
     	ThreadLauncher.jeu = this;
     	ThreadLauncher.LancerMusique();
     	if(Sauvegarde.Deserialize(partie)!=null) {
-    		System.out.println("javais une partie");
+    		// Deserialize renvoie null si il n'a pas trouvé de partie. Donc, on ne 
+    		// rentre dans cette boucle que si on trouve une partie.
     		Partie savePartie = Sauvegarde.Deserialize(partie);
     		this.partie = savePartie;
-//    		partie.setZoneCourante(savePartie.getZoneCourante());
     		gui.addNameFrame(partie.getJoueur().getNom()); 
     		gui.addAllActionListener();
     		afficherLocalisation();
+    		//La sauvegarde n'est possible que si un joueur a été crée, il faut donc relancer
+    		//les threads propre au joeur.
     		lancerThreadJoueur();
     	} else {
-    		System.out.println("coucou je suis là");
     		lancerNouvellePartie();
     	}
     }
+    /**
+     * Lance une nouvelle partie.
+     */
     public void lancerNouvellePartie() {
     	partie = new Partie();
 	    creerCarte();
 	    afficherMessageDeBienvenue();
 		sceneOuverture();
     }
+    /** Fonction utilisé avec la première entrée du texte pour créer un joueur
+     * @param String nomJoueur
+     */
     public void creationJoueur(String nomJoueur) {
     	partie.setJoueur(new Joueur(nomJoueur));
     	gui.addNameFrame(nomJoueur);
     	lancerThreadJoueur();
     }
+    /**
+     *  Lance une scène d'ouverture pour lancer l'énigme.
+     */
     public void sceneOuverture() {
-    	boolean inProgress = true;
     	gui.afficher(partie.getGuideDuJeu().dialogueLancementQuete(),partie.getGuideDuJeu());
     	while(partie.getJoueur()==null) {
-    		
+    		// Permet d'attendre la création du joueur avant la suite de la scène d'ouverture
     	}
-    	while(inProgress) {
     		gui.afficher(partie.getGuideDuJeu().dialoguePendantQuete(0).replaceAll("joueur", partie.getJoueur().getNom()),partie.getGuideDuJeu());
     		try {
 				Thread.sleep(10000);
@@ -68,10 +83,10 @@ public class Jeu implements Serializable {
     		gui.afficher(partie.getGuideDuJeu().dialoguePendantQuete(1));
     		getPartie().getJoueur().prendreObjet(new Teleporteur("Teleporteur", "Vieux téléporteur, peu fiable...", "teleporteur.png",3));
     		gui.addAllActionListener();
-    		inProgress = false;
-    	}
-    	System.out.println("Bienvenue chez les Hyènes");
     }
+    /**
+     * Crée la carte
+     */
     private void creerCarte() {
     	ArrayList<Zone> zones = WorldBuilder.creerToutesLesZones();
     	zones = WorldBuilder.ajouterToutesLesSorties(zones);
@@ -83,18 +98,17 @@ public class Jeu implements Serializable {
     	ArrayList<Objets> objets = WorldBuilder.creerLesObjets();
     	ArrayList<Quete> quetes = WorldBuilder.creerLesQuetesDuJeu(objets);
     	zones = WorldBuilder.miseEnPlaceDesQueteurs(zones,quetes);
+    	// Set les zones à sauvegarder dans la partie car elles seront cruciales pour le jeu
     	partie.setGuideDuJeu(WorldBuilder.CreerGuide());
     	partie.setSalleDeRepos(WorldBuilder.ajouterSortieZoneDeRepos(zones.get(1), "SUD", vaisseau));
     	partie.setsceneFinal(zones.get(zones.size()-1));
     	partie.setEspace(espace);
     	partie.setZoneCourante(espace.get(0).getZones().get(1));
     	partie.setVaisseau(zones.get(0));
-    	System.out.println(espace.get(0).getZones().get(1).getNom());
-//    	Thread music = new Thread(new Music("dust.wav"));
-//    	music.start();
     }
-    
-
+    /**
+     * Méthode utilisé pour afficher toutes les éléments de l'IHM constituant la zone courante
+     */
     void afficherLocalisation() {
     	gui.afficheImage(partie.getZoneCourante().nomImage());	
         gui.afficher(partie.getZoneCourante().descriptionLongue());
@@ -105,7 +119,9 @@ public class Jeu implements Serializable {
             gui.afficherInventaire(getPartie().getJoueur().inventaire);
         }
     }
-    
+    /**
+     * Affiche le message de bienvenenue
+     */
     private void afficherMessageDeBienvenue() {
         gui.afficher("Cliquez sur 'Aide' dans le menu 'Joueur' pour obtenir de l'aide.");
         gui.afficheImage(partie.getZoneCourante().nomImage());	
@@ -113,91 +129,125 @@ public class Jeu implements Serializable {
         afficherLocalisation();
     }
 
+    /** Assure que le joueur attrape le mouton selon qu'il lui reste ou non de la place dans son inventaire
+     * @param mouton
+     */
     public void captureDeMouton(Mouton mouton) {
-    	partie.getJoueur().prendreObjet(mouton);
-    	WorldBuilder.suppresionDuMouton(partie.getZoneCourante(), mouton);	
+    	if(partie.getJoueur().inventaire.size()<8) {
+        	partie.getJoueur().prendreObjet(mouton);
+        	WorldBuilder.suppresionDuMouton(partie.getZoneCourante(), mouton);
+    	}	
     }
-    public void allerEn(String direction) {
-    	Zone nouvelle = partie.getZoneCourante().obtientSortie(direction);
-    	if ( nouvelle == null ) {
-        	gui.afficher( "Pas de sortie " + direction);
-    		gui.afficher();
-    	}
-        else {
-        	partie.setZoneCourante(nouvelle);
-        	afficherLocalisation();
-        }
-    }
-    
+	/** Méthode qui déplace le joueur
+	 * @param Zone nouvelleZone
+	 */
 	public void allerEn(Zone nouvelleZone) {
     	partie.setZoneCourante(nouvelleZone);
         afficherLocalisation();
     }
+    /** Méthode permettant de gérer l'interraction avec les personnages
+     * @param personnage
+     */
     public void interractionPersonnage(Personnage personnage) {
     	if(personnage instanceof Allies) {
     		gui.afficher(personnage.parler(),personnage);
     		//friends est utilisé dans le but d'avoir un HashSet empêchant d'aoir plusieurs fois les mêmes alliés
     		//sur la zone de combat final ou sur la salle de repos.
     		partie.getJoueur().friends.add(personnage);
+    		// Si il n'y a pas de différence dans la taille, c'est que l'alliée à qui je viens de parler est 
+    		// déjà dans ma salle d'amis/
     		if(partie.getJoueur().friends.size()!=partie.getSalleDeRepos().getPersonnageDansLaZone().size()) {
     			partie.getSalleDeRepos().getPersonnageDansLaZone().add(personnage);
     			partie.getSceneFinal().getPersonnageDansLaZone().add(personnage);
-    		}
+    		} // Maintenant si on est face à un quêteur : 
     	} else if (personnage instanceof Queteur) {
+    		// On récupère le personnage pour le cast sous forme de quêteur
     		Queteur queteur = (Queteur) personnage;
     		if(!queteur.besoinAide()) {
+    			// On vérifie si on a déjà aidé le quêteur, ici il a besoin d'aide
     			if(getPartie().queteEnCoursPartie()==null) {
+    				// Ici on vérifie qu'on a aucune quête et qu'on peut venir aider le quêteur
     				partie.setQuete((queteur).quete());
+    				// Quand on fait parler le quêteur on lance notre quête avec le dialogue de début
     				gui.afficher(queteur.parler(getPartie().getJoueur()),queteur);
-    				System.out.println(queteur.quete().getClass());
-    			} else {
+    			} else { // Si on rentre ici c'est que le quêteur a besoin d'aide MAIS qu'on a une quête en cours
     				if(queteur.quete()==getPartie().queteEnCoursPartie()) {
+    					// Ici le quêteur auquel on parle est celui dont on est en train d'effectuer la quête
+    					// Donc on intéragit avec lui dans le cadre de la quête
     					gui.afficher(queteur.quete().executerQuete(getPartie().getJoueur(), queteur));
     				} else {
+    					// Ici j'ai une quête en cours mais pas celle du quêteur auquel je parle, il va
+    					// donc me dire qu'il est occupé.
     					gui.afficher(((Queteur) personnage).queteDejaEnCours(),queteur);
     				}
     				
     			}
 			} else {
+				// Ici le quêteur n'a pas besoin d'aide, donc cela veut dire que je l'ai déjà aidé, il me remercie
 				gui.afficher(queteur.remerciement(),queteur);
 			}
     	} 
     }
+    /** getter de la zone courante
+     * @return Zone zonecourante
+     */
     public Zone getZoneCourante() {
     	return this.getZoneCourante();
     }
+    /**
+     * Permet d'incrémenter la zone courante
+     */
     public void incrementerCommande() {
     	partie.nbCommande++;
-    	System.out.println(partie.nbCommande);
     	if(partie.nbCommande==partie.nbCommandeMax) {
     		perdu();
     	}
     }
+    /**
+     * Le jeu est perdu, on affiche la zone perdante.
+     */
     public void perdu() {
-    	System.out.println("T'as perdu wesh! ");
     	afficherScenePerdante();
     }
+    /**
+     * Création et affichage d'une zone perdante.
+     */
     public synchronized void afficherScenePerdante() {
     	partie.setZoneCourante(new Zone("","zoneperdante.gif","Vous êtes mort. Sans votre aide, Dyspros continuera encore longtemps son règne de terreur sur la galaxie..."));
     	afficherLocalisation();
     }
-    public void SupprimerPartie()
-    {
+    /**
+     * Permet de supprimer la partie et donc la sauvegarde/
+     */
+    public void SupprimerPartie() {
 		File fichier = new File("src/data/sauvegarde.txt");
 		fichier.delete();
 		gui.stopFenetre();
     }
+    /**
+     * Lance tous les Threads en lien avec le joueur
+     */
     public void lancerThreadJoueur() {
 		ThreadLauncher.checkPhaseOfGame();
 		ThreadLauncher.checkLifeJoueur();
     }
-	public void envoyerReponseEnigme(String str,Queteur queteur) {
+    
+	/** Envoie réponse à l'énigme en cours
+	 * @param String réponse
+	 * @param queteur
+	 */
+	public void envoyerReponseEnigme(String reponse,Queteur queteur) {
 		if(queteur.quete() instanceof Pendu) {
-			gui.afficher(((Pendu)queteur.quete()).executerQuete(getPartie().getJoueur(), queteur,str));
+			// Ici, la réponse est envoyé pour exécuter le pendu
+			gui.afficher(((Pendu)queteur.quete()).executerQuete(getPartie().getJoueur(), queteur,reponse);
 		} else if(queteur.quete() instanceof EnigmeTextuel) {
-			gui.afficher(((EnigmeTextuel)queteur.quete()).executerQuete(getPartie().getJoueur(), queteur,str));
+			// Ici la réponse est envoyé pour exécuter l'Enigme Textuelle
+			gui.afficher(((EnigmeTextuel)queteur.quete()).executerQuete(getPartie().getJoueur(), queteur,reponse));
 		}
 	}
+	/**
+	 * Lance la phase finale avec le retournement de situation, ajoute la sortie pour la zone de combat
+	 */
 	public void lancerPhaseFinale() {
 		partie.getSalleDeRepos().ajouteSortie(Sortie.valueOf("NORD"), partie.getSceneFinal());
 		partie.setZoneCourante(partie.getSalleDeRepos());
@@ -211,18 +261,28 @@ public class Jeu implements Serializable {
 		afficherLocalisation();
 		gui.afficher(partie.getGuideDuJeu().dialogueFinQuete().replaceAll("Joueur",partie.getJoueur().getNom()));
 	}
+	/**
+	 * Sauvegarde
+	 */
 	public void Sauvegarde() { Sauvegarde.Serialize(partie); }
+	/** Gérer l'apparation ou disparition du méchant selon le boolean passé en paramètre
+	 * @param isDisplayed ( True = Apparait / False = disparait )
+	 */
 	public void apparitionMechant(boolean isDisplayed) { gui.displayDyspros(partie.dyspros().getImage(),isDisplayed); }
-	
+	/** Affiche le remerciement de Mentris selon le niveau
+	 * @param int cptNiveau
+	 */
 	public void mentrisRemerciement(int cptNiveau) {
 		// TODO Auto-generated method stub
 		partie.setZoneCourante(partie.getVaisseau());
 		afficherLocalisation();
 		gui.afficher(partie.getGuideDuJeu().dialoguePendantQuete(cptNiveau).replaceAll("joueur", getPartie().getJoueur().getNom()));
 	}
+	/** Exécute le tour du combat 
+	 * @param Personnage personnage attaquant avec le joueur pour ce tour
+	 */
 	public void tourDuComabat(Allies personnage) {
 		partie.getJoueur().attaquer(partie.dyspros()); 
-		gui.afficher("LES PV DE DYSPROS SONT DE "+partie.dyspros().getPointDeVie());
 		if(partie.dyspros().getPointDeVie()<=0) {
 			partie.setZoneCourante(new Zone("Galaxie","win.gif","Vous avez sauvé toute votre galaxie"));
 			gui.afficherCredit();
@@ -240,15 +300,24 @@ public class Jeu implements Serializable {
 			partie.getJoueur().setPointDeVie(0);
 		}
 		gui.afficherElementZone(partie.getZoneCourante().getAnimauxDansLazone(), partie.getZoneCourante().getPersonnageDansLaZone());
-		gui.afficher("LES PV QUE TU AS"+partie.getJoueur().getPointDeVie());	
+		gui.afficher("LES PV DE DYSPROS SONT" + partie.dyspros().getPointDeVie()+ "\n LES PV QUE TU AS"+partie.getJoueur().getPointDeVie());	
 	}
+	/** Retire le perosnnage mort de la zone de combat
+	 * @param Personnage personnagemort
+	 */
 	private void retirerPersonnageMort(Allies personnage) {
 		// TODO Auto-generated method stub
 		partie.getZoneCourante().getPersonnageDansLaZone().remove(personnage);
 	}
+	/**
+	 * Lance le combat en changeant les MouseListener des label des personnages
+	 */
 	public void lancerCombat() {
 		gui.addActionListenerCombat();
 	}
+	/**
+	 * Téléporte le joueur dans une zone aléatoire
+	 */
 	public void teleporterJoueur() {
 		Objets obj = partie.getJoueur().inventaire.get(0);
 		if(obj instanceof Teleporteur) {
@@ -258,6 +327,5 @@ public class Jeu implements Serializable {
 				afficherLocalisation();
 			}
 		}
-		
 	}
 }
